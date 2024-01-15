95995f8446930f265e3fd4b543692a010f27b1ab
Jura <iuriigushchin@yandex.ru>
2024-01-15 11:41:24 +0300

    frontend: create Nom page

---
+++ frontend/farm-app/src/routes/createNomenculature.js
@@ -0,0 +1,57 @@
+import * as React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Link from "@mui/material/Link";
+import Orders from "./Orders";
+import AppBar from '@mui/material/AppBar';
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}>
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Medilon
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const defaultTheme = createTheme();
+
+export default function Dashboard() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            sx={{ flexGrow: 2 }}>
+            Создание номенкулатуры
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+            <Orders />
+          </Paper>
+        </Grid>
+      </Grid>
+      <Copyright sx={{ pt: 4 }} />
+    </ThemeProvider>
+  );
+}
